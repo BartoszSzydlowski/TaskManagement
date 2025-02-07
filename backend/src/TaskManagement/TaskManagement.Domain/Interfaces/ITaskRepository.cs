f104251acf24f38a639ae4b10422c832fe7a95c3
BARTEK <bbartek311@gmail.com>
2025-02-07 21:16:56 +0100

    Added task repository

---
+++ backend/src/TaskManagement/TaskManagement.Domain/Interfaces/ITaskRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManagement.Domain.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<List<Models.Task>> GetAll();
+
+        Task<Models.Task> Get(int id);
+
+        Task Add(Models.Task task);
+
+        Task Update(Models.Task task);
+
+        Task Delete(int id);
+    }
+}
---
+++ backend/src/TaskManagement/TaskManagement.Infrastructure/Repository/TaskRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskManagement.Domain.Interfaces;
+
+namespace TaskManagement.Infrastructure.Repository
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly List<Domain.Models.Task> _tasks;
+
+        public TaskRepository()
+        {
+            _tasks =
+            [
+                new() { Id = 1, Difficulty = 1, TaskType = { Id = 1, TaskName = "Wdrożenie" }, Text = "Text" }
+            ];
+        }
+
+        public Task<List<Domain.Models.Task>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Domain.Models.Task> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Add(Domain.Models.Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Domain.Models.Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
