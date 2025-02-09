3ab73722809d5f05b93e2d11f7dee9bb823bf9fd
BARTEK <bbartek311@gmail.com>
2025-02-09 01:26:27 +0100

    Code cleanup

--- backend/src/TaskManagement/TaskManagement.Infrastructure/Repository/UserRepository.cs
+++ backend/src/TaskManagement/TaskManagement.Infrastructure/Repository/UserRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TaskManagement.Domain.Interfaces;
+﻿using TaskManagement.Domain.Interfaces;
 using TaskManagement.Domain.Models;
 using Task = System.Threading.Tasks.Task;
 
