c5dd8b20ceea5eb34494faaf78701698516fb4ae
BARTEK <bbartek311@gmail.com>
2025-02-12 03:04:16 +0100

    added services

--- frontend/src/TaskManagement/src/app/services/task.service.ts
+++ frontend/src/TaskManagement/src/app/services/task.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../models/task.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class TaskService {
+  private apiUrl = '/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getDeploymentTasks(params: any): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}/DeploymentTasks/GetFilteredByTaskTypeAndSortedByDifficultyDesc`, { params });
+  }
+
+  getImplementationTasks(params: any): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}/ImplementationTasks/GetFilteredByTaskTypeAndSortedByDifficultyDesc`, { params });
+  }
+
+  getMaintenanceTasks(params: any): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}/MaintenanceTasks/GetFilteredByTaskTypeAndSortedByDifficultyDesc`, { params });
+  }
+
+  addTaskToUser(taskIds: number[], userId: number): Observable<any> {
+    return this.http.post(`${this.apiUrl}/Tasks/AddTaskToUser`, { tasksIds: taskIds, userId });
+  }
 }
